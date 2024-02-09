3d52668510518b7e7a469ae78e896c9610ac9d81
unknown <chesusar@gmail.com>
2024-02-09 12:30:17 +0100

    final practicas

--- 7_sensor_i2c/7_sensor_i2c.ino
+++ 7_sensor_i2c/7_sensor_i2c.ino
@@ -1,6 +1,9 @@
 #include <Arduino_LSM9DS1.h>
 #include "BBTimer.hpp"
 
+#define MASTER
+// #define PERIPHERAL
+
 BBTimer tim0(BB_TIMER0);
 
 bool readEvent = false;
@@ -8,11 +11,25 @@ bool printEvent = false;
 
 void setup() {
   Serial.begin(9600);
+#ifdef PERIPHERAL
+  Wire.begin(8);
+#endif
+#ifdef MASTER
+  Wire.begin();
+#endif
   IMU.begin();
   tim0.setupTimer(200000, tim0Callback);  // 200 ms
 }
 
 void loop() {
+#ifdef PERIPHERAL
+  receive();
+  if (printEvent){
+    analogWrite(LED_BUILTIN, 0);
+  }
+#endif
+
+#ifdef MASTER
   static float acelX, acelY, acelZ;
   static float gyroX, gyroY, gyroZ;
   static float magX, magY, magZ;
@@ -42,8 +59,15 @@ void loop() {
 
     tim0.timerStop();
 
-    char buff[128];
-    sprintf(buff, "Acel (%.3f, %.3f, %.3f)\nGyro (%.3f, %.3f, %.3f)\nMag (%.3f, %.3f, %.3f)",
+    const int BUFF_SIZE = 128;
+    char buff[BUFF_SIZE];
+
+    for (int i = 0; i < BUFF_SIZE; i++){
+      buff[i] = ' ';
+    }
+    buff[BUFF_SIZE - 1] = '\n';
+
+    sprintf(buff, "Acel (%.3f, %.3f, %.3f)\nGyro (%.3f, %.3f, %.3f)\nMag (%.3f, %.3f, %.3f)\n",
             acelX, acelY, acelZ,
             gyroX, gyroY, gyroZ,
             magX, magY, magZ);
@@ -52,12 +76,23 @@ void loop() {
 
     send(buff, 128);
   }
+#endif
 }
 
 void send(char *buff, int size) {
+  for (int i = 0; i < size; i++) {
+    Wire.beginTransmission(8);
+    Wire.write(buff[i]);
+    Wire.endTransmission();
+  }
 }
 
 void receive() {
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+    analogWrite(LED_BUILTIN, 255);
+  }
 }
 
 void tim0Callback() {
